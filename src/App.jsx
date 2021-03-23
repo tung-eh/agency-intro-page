09ce224493f0ae47d0ebf23f314941b75d816c82
Tung Teng <thetungctn@gmail.com>
2021-03-24 05:35:00 +0700

    Install & Config theme-ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { Box, Heading, ThemeProvider } from 'theme-ui'
 import React from 'react'
 
+import theme from './theme'
+
 function App() {
-  return <h1>Hello world</h1>
+  return (
+    <ThemeProvider theme={theme}>
+      <Box m={10}>
+        <Heading color="tomato">Hello world</Heading>
+      </Box>
+    </ThemeProvider>
+  )
 }
 
 export default App
