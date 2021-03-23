424812b34b8981bf572ce9ff85a7b4e181c1e219
Tung Teng <thetungctn@gmail.com>
2021-03-24 04:48:40 +0700

    Config eslint & prettier & importjs

---
+++ .importjs.js
@@ -0,0 +1,18 @@
+module.exports = {
+  namedExports: {
+    react: [
+      'useState',
+      'useReducer',
+      'useEffect',
+      'useLayoutEffect',
+      'useContext',
+      'useRef',
+      'useCallback',
+      'useMemo',
+      'forwardRef',
+      'createContext',
+      'cloneElement',
+      'memo',
+    ],
+  },
+}
