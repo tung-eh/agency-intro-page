f8dcf6fcfaef56fb6a38164d2131a1bc181065cc
Tung Teng <thetungctn@gmail.com>
2021-03-26 04:39:29 +0700

    Intro overlay

---
+++ src/App/Intro.jsx
@@ -0,0 +1,67 @@
+import { Box } from 'theme-ui'
+import React from 'react'
+
+const OverlayTop = ({ sx }) => (
+  <Box
+    sx={{
+      position: 'absolute',
+      height: '100%',
+      width: ['100vw', '100vw', 'calc(100vw / 3)'],
+      bg: 'black',
+      bottom: 0,
+      left: 0,
+      ':nth-child(2)': {
+        left: 'calc(100vw /  3)',
+      },
+      ':nth-child(3)': {
+        left: 'calc(100vw * 2 / 3)',
+      },
+      ':nth-child(n + 2)': {
+        display: ['none', 'none', 'block'],
+      },
+    }}
+  />
+)
+
+const OverlayBottom = () => (
+  <Box
+    sx={{
+      position: 'absolute',
+      height: '100%',
+      width: ['100vw', '100vw', 'calc(100vw / 3)'],
+      bg: 'black',
+      bottom: 0,
+      right: [0, 0, 'calc(100vw * 2 / 3)'],
+      zIndex: 10,
+      ':nth-child(2)': {
+        right: [0, 0, 'calc(100vw / 3)'],
+        top: ['100%', '100%', 'unset'],
+      },
+      ':nth-child(3)': {
+        right: 0,
+        top: ['200%', '200%', 'unset'],
+      },
+    }}
+  />
+)
+
+const Intro = () => {
+  return (
+    <Box>
+      <Box sx={{ height: '50vh', position: 'absolute', width: '100%' }}>
+        <OverlayTop />
+        <OverlayTop />
+        <OverlayTop />
+      </Box>
+      <Box
+        sx={{ height: '50vh', position: 'absolute', width: '100%', bottom: 0 }}
+      >
+        <OverlayBottom />
+        <OverlayBottom />
+        <OverlayBottom />
+      </Box>
+    </Box>
+  )
+}
+
+export default Intro
--- src/App/index.jsx
+++ src/App/index.jsx
@@ -4,11 +4,13 @@ import React from 'react'
 import Banner from './Banner'
 import Cases from './Cases'
 import Header from './Header'
+import Intro from './Intro'
 import theme from '../theme'
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
+      <Intro />
       <Header />
       <Banner />
       <Cases />
