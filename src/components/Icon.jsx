78f934a8c0faf04121ee5bfbbd2534a30e73bbb2
Tung Teng <thetungctn@gmail.com>
2021-03-29 07:34:48 +0700

    Add arrow-up-circle icon

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import { Box } from 'theme-ui'
 import { ReactComponent as ArrowLeft } from '../icons/arrow-left.svg'
 import { ReactComponent as ArrowRight } from '../icons/arrow-right.svg'
+import { ReactComponent as ArrowUpCircle } from '../icons/arrow-up-circle.svg'
 const MAP = {
   'arrow-left': ArrowLeft,
   'arrow-right': ArrowRight,
+  'arrow-up-circle': ArrowUpCircle,
 }
 const Icon = ({ name, ...props }) => {
   const Component = MAP[name]
